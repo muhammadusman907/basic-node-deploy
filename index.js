e3fb30a9fe33dd7e79362ddefb8710c42b19af9c
Muhammad usman <122649540+muhammadusman907@users.noreply.github.com>
2024-04-06 16:13:34 +0500

    changes

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors"
 const app = express();
 app.use(cors)
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

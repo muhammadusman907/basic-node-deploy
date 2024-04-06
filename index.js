d4050102389d8865639646337611fe9e99981b8c
Muhammad usman <122649540+muhammadusman907@users.noreply.github.com>
2024-04-06 16:47:24 +0500

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   try {
     res.status(200).send("Express on Vercel");
